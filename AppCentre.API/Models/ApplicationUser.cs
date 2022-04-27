efba8934c4d0687b0f9154fbde1556d0d3fa7c18
Youcef <Youcef.Oulddjaballah2x@gmail.com>
2022-04-27 19:31:38 +0100

    Add AppUser and AppDbCtxt
    costumize AppUser with new attrebutes

---
+++ AppCentre.API/Models/ApplicationUser.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace AppCentre.API.Models
+{
+    public class ApplicationUser:IdentityUser
+    {
+        [Required,MaxLength(5)]
+        public string Matricule { get; set; }
+        [Required]
+        public int NN { get; set; }
+        [Required, MaxLength]
+        public int Grade { get; set; }
+        [Required, MaxLength(50)]
+        public string Service { get; set; }
+        [Required, MaxLength(50)]
+        public string Nom { get; set; }
+        [Required, MaxLength(50)]
+        public string Prenom { get; set; }
+        [Required, MaxLength(50)]
+        public string ApplicationName { get; set; }
+
+    }
+}
